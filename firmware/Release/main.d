3c8d7a77021b5d0fd2c42388f2da7915c3b17271
etherkit <jason@etherkit.com>
2012-05-15 20:56:37 -0700

    Initial commit

---
+++ firmware/Release/main.d
@@ -0,0 +1,10 @@
+main.d main.o: ../main.c ../usbdrv/usbdrv.h ../usbdrv/../usbconfig.h \
+ ../usbdrv/usbportability.h ../usbdrv/oddebug.h
+
+../usbdrv/usbdrv.h:
+
+../usbdrv/../usbconfig.h:
+
+../usbdrv/usbportability.h:
+
+../usbdrv/oddebug.h:
